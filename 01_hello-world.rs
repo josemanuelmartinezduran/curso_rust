1debe915d8da28eeb217d689dd11267cac7ad5fb
Jose Manuel <josemanuelmartinezduran@gmail.com>
2023-08-07 19:46:39 -0600

    Clase 1 ejercicios 1 y 2

---
+++ 01_hello-world.rs
@@ -0,0 +1,21 @@
+// Comentario
+
+
+// Función main
+fn main() {
+    // Comentario de una línea
+
+    /*
+     * Comentario
+     * multi
+     * línea
+     */
+
+    /*
+    No es necesario poner asteriscos en todos lados
+    */
+
+    // Función print
+    println!("Hola Mundo!");
+}
+
