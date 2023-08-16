56aa0c2b93a158b2cebc45c7acf734f88bc4fa9a
Jose Manuel <josemanuelmartinezduran@gmail.com>
2023-08-15 19:50:09 -0600

    Traits

---
+++ 26_ejemplo.rs
@@ -0,0 +1,17 @@
+struct Ave {
+    especie:String
+}
+
+impl Ave {
+    fn new(especie:String)-> Self{
+        Self{especie: format!("{}",especie)}
+    }
+    
+    fn imprime(a:&Ave){
+        println!("La especie del ave es {}", a.especie);
+    }
+}
+
+fn main(){
+   let a = Ave{especie: "Gaviota".to_string()};
+}
